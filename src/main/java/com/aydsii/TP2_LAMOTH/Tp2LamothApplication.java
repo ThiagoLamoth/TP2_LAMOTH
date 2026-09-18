f7da87c0407add1d355822de9969703542424f03
ThiagoLamoth <thiagolamoth35@gmail.com>
2026-09-18 14:31:47 -0300

    primer commit

---
+++ src/main/java/com/aydsii/TP2_LAMOTH/Tp2LamothApplication.java
@@ -0,0 +1,13 @@
+package com.aydsii.TP2_LAMOTH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2LamothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2LamothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aydsii/TP2_LAMOTH/Tp2LamothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aydsii.TP2_LAMOTH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2LamothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
